e60206f39e90920c004f0d3e22d3fb44e69e2a9a
Jay Sprout <jotasprout@gmail.com>
2022-07-10 15:01:09 -0400

    rebuilding UI

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -8,6 +8,13 @@ public class MainMenu
         {
             Console.WriteLine("Welcome to AutumnERS, where expenses are as beautiful as Autumn leaves.");
             Console.WriteLine("Here is a list of all our users:");
+            UserDAO peeps = new UserDAO();
+            peeps.GetAllUsers();
+
+            Console.WriteLine("Here is a list of what they want you to pay for:");
+
+            TicketDAO claims = new TicketDAO();
+            claims.GetAllTickets();
         } while (true);
     }
 }
